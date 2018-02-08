26d76cab7d0b3d0bc6d2dff1cb0f10e738e026d9
osivwi <osivwiokiti@gmail.com>
2018-02-08 03:01:03 +0100

    Update squirrel.php

--- squirrel.php
+++ squirrel.php
@@ -3,70 +3,111 @@ namespace squirrel;
 use PDO;
 class squirrel
 {
-  static $handler = null;
-  function __construct()
+  var $handler = null;
+  static $instance = null;
+  private function __construct()
   {
-    # code...
+    //Defualt constructor..
   }
-  public function connect($dbname, $username, $password, $host='127.0.0.1') {
-    // this function is used for connecting to the database...
-    // You are expected to pass the connection details as an array.
-    try
-    {
-      self::$handler = new PDO('mysql:host = '.$host.'; dbname='.$dbname, $username, $password);
-      self::$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e)
-    {
-      die('Unable to connect to the database');
-    };
-      return self::$handler;
-  }
-  function disconnect(){
-    return self::$handler = null;
-  }
-  public static function insert($tablename,$array)
+  public static function getInstance()
   {
-    // array('firstname'=>'john', 'lastname'=>'doe');
-    $columnString = implode(',', array_keys($array));
-    $valueString = implode(',', array_fill(0, count($array), '?'));
+    if (null == self::$instance)
+    {
+      self::$instance = new static();
+    }else{}
+      return self::$instance;
+  }
+  public function connect($dbname, $username, $password, $host='127.0.0.1')
+  {
+      try
+      {
+        $this->handler = new PDO('mysql:host = '.$host.'; dbname='.$dbname, $username, $password);
+        $this->handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $e)
+      {
+        die('Unable to connect to the database');
+      };
+      return $this->handler;
+  }
+  public function disconnect()
+  {
+    return $this->handler = null;
+  }
+  /*
+    insert values into the database
+    Parameters:
+        $tablename - String
+        $values - Array
+    E.g:
+        $tablename - 'users'
+        $values - array('firstname'=>'john', 'lastname'=>'doe');
+*/
+  public function insert($tablename, $values)
+  {
+    $columnString = implode(',', array_keys($values));
+    $valueString = implode(',', array_fill(0, count($values), '?'));
     $sql = "INSERT INTO ".$tablename." ({$columnString}) VALUES ({$valueString})";
-    $query = self::$handler->prepare($sql);
-    if($query->execute(array_values($array)))
+    $query = $this->handler->prepare($sql);
+    if($query->execute(array_values($values)))
     {
       return true;
     }else{
       return false;
     }
   }
-  public static function update($tablename,$row,$id,$array)
+  /*
+    Function to update values in database
+    Parameters:
+        $Tablename - String
+        $column - String
+        $id - String/Int
+        $values - Array
+    E.g
+        $tablename - 'users'
+        $columns - 'id'
+        $id - 1
+        $values - array('firstname'=>'john', 'lastname'=>'doe');
+*/
+  public function update($tablename, $column, $id, $values)
   {
-    $columns = implode(',', array_keys($array));
-    $columns = explode(',', $columns);
-    $values = implode(',', array_values($array));
-    $values = explode(',', $values);
-    $na; $columnString=array();
-    $valueArray = array();
-    for($i=0; $i<count($columns); $i++)
-    {
-      $na = $columns[$i]."=?";
-      array_push($columnString, $na);
-      array_push($valueArray,$values[$i]);
-    }
-    $cstring = implode(',',$columnString);
-    array_push($valueArray, $id);
-    $sql = "UPDATE ".$tablename." SET ".$cstring." WHERE ".$row." = ?";
-    $query = self::$handler->prepare($sql);
-    if($query->execute($valueArray))
-    {
-      return true;
-    }else{
-      return false;
-    }
+      $columns = implode(',', array_keys($values));
+      $columns = explode(',', $columns);
+      $values = implode(',', array_values($values));
+      $values = explode(',', $values);
+      $na; $columnString = array();
+      $valueArray = array();
+      for ($i=0; $i<count($columns); $i++) {
+          $na = $columns[$i]."=?";
+          array_push($columnString, $na);
+          array_push($valueArray,$values[$i]);
+      }
+      $cstring = implode(',', $columnString);
+      array_push($valueArray, $id);
+      $sql = "UPDATE " . $tablename . " SET " . $cstring . " WHERE " . $column . " = ?";
+      $query = $this->handler->prepare($sql);
+
+      if ($query->execute($valueArray)) {
+          return true;
+      } else {
+          return false;
+      }
   }
-  public static function delete($tablename,$row,$id)
+  /*
+    Function to delete a row from the database
+    Parameters:
+        $tablename - String
+        $column - String
+        $id - String/Int
+
+    E.g:
+        $tablename - 'users'
+        $column - ID
+        $id - 3
+*/
+  public function delete($tablename, $column, $id)
   {
-    $sql = "DELETE FROM ".$tablename." WHERE ".$row." = ?";
-    $query  = self::$handler->prepare($sql);
+    $sql = "DELETE FROM ".$tablename." WHERE ".$column." = ?";
+    $query  = $this->handler->prepare($sql);
     if($query->execute(array($id)))
     {
       return true;
@@ -76,28 +117,47 @@ class squirrel
       return false;
     }
   }
-  public function selectAll($tablename)
+  /*
+    Get all rows from the database table
+    Parameters:
+        $tablename: String
+    E.g
+        $tablename - 'users'
+*/
+  public function selectAll($tablename, $order = "DESC")
 	{
 		$sql = "SELECT * FROM ".$tablename." ";
-		$sql .= " ORDER BY `id` DESC  ";
-		$this->query =$this->handler->query($sql);
-		return $this->query;
+		$sql .= " ORDER BY `id` ".$order."  ";
+		$query = $this->handler->query($sql);
+		return $query;
 	}
+  /*
+    Get row from the database table by id
+    Parameters:
+        $tablename: String
+        $id: Int
+    E.g
+        $table - 'users'
+        $id - 1
+*/
 	public function selectById($table,$id)
 	{
 		if (isset($id))
 		{
 			$sql = "SELECT * FROM ".$table." WHERE id=?  LIMIT 1";
-			$this->query = $this->handler->prepare($sql);
-			$this->query->execute(array($id));
-			return $this->query;
+			$query = $this->handler->prepare($sql);
+			$query->execute(array($id));
+			return $query;
 		}
 		else
 		{
 			return false;
 		}
 	}
-	public function selectColumn($table,$column)
+  /*
+    Select an entire column from a database.
+  */
+	public function selectColumn($table, $column)
 	{
 		if (isset($column) && isset($table))
 		{
@@ -107,14 +167,17 @@ class squirrel
 			return $query;
 		}	else{return false;	}
 	}
-	public function Find($table,$column,$name)
+  /**
+  * function to find a particlar row in a databasee.
+   */
+	public function Find($table, $column ,$name)
 	{
 		if (isset($name))
 		{
 			$sql = "SELECT * FROM ".$table." WHERE ".$column."=?  ORDER BY id DESC";
-			$this->query = $this->handler->prepare($sql);
-			$this->query->execute(array($name));
-			return $this->query;
+			$query = $this->handler->prepare($sql);
+			$query->execute(array($name));
+			return $query;
 		}	else{	return false; }
+  }
 }
-?>
